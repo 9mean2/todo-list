7d0b868e572314e81681ad8326ad20925d1c5153
9mean2 <o3omoomin@gmail.com>
2023-02-06 15:51:28 +0900

    feat 제목과 내용 추가 버튼과 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState([
+    { id: 0, title: "숨쉬기운동", body: "숨을 마셔보자", isDone: false },
+  ]);
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const bodyChangeHandler = (event) => {
+    setBody(event.target.value);
+  };
+
+  //추가 버튼 핸들러
+  const buttonClickHandler = () => {
+    const newTodo = {
+      id: todo.length + 1,
+      title: title,
+      body: body,
+    };
+
+    setTodo([...todo, newTodo]);
+  };
+
+  const deleteButtonClickHandler = (id) => {
+    const newTodo = todo.filter((todo) => todo.id !== id);
+    setTodo(newTodo);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="back">
+      <div className="layout">
+        <div className="nav">
+          <label>My todo list</label>
+        </div>
+
+        <div className="add">
+          제목 <input value={title} onChange={titleChangeHandler} />
+          내용 <input value={body} onChange={bodyChangeHandler} />
+          <br />
+          <button
+            type="button"
+            class="btn btn-success"
+            onClick={buttonClickHandler}
+          >
+            추가하기
+          </button>
+        </div>
+        <div className="app-style">
+          {todo.map(function (item) {
+            return (
+              <User
+                key={item.id}
+                item={item}
+                deleteButtonClickHandler={deleteButtonClickHandler}
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
+
+const User = ({ item, deleteButtonClickHandler }) => {
+  return (
+    <div key={item.id} className="component-style">
+      {item.body} - {item.title}{" "}
+      <button onClick={() => deleteButtonClickHandler(item.id)}>x</button>
+    </div>
+  );
+};
 
 export default App;
